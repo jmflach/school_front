649c81b430526ba03787085c1dc2618dcaf52d2e
joao <joaoflach@gmail.com>
2023-07-02 10:13:43 -0300

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Students from "./pages/Students";
 import Home from "./pages/Home";
 import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import classes from './Footer.module.css'
+
+function Footer () {
+    return <footer className={classes.footer}>
+        <h3 className={classes.logo}>Desenvolvido por João Flach</h3>
+    </footer>
+}
+
+export default Footer;
