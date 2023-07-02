84046952f37e9eb0193b1c68cc4dce4664b3d0f0
joao <joaoflach@gmail.com>
2023-07-01 21:57:08 -0300

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import Students from "./pages/Students";
 import Home from "./pages/Home";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/students" element={<Students />} />
-    </Routes>
+    <div>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+      </Routes>
+    </div>
   );
 }
 
