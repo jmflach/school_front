bd5415a4cbe8b6f73b6e4c3f6782103b4147ae31
joao <joaoflach@gmail.com>
2023-07-01 22:35:32 -0300

    homepage ready

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,16 @@
-function Home () {
-    return <div>Home Page</div>
+import logo from "../img/home.jpg";
+import classes from "./Home.module.css";
+
+function Home() {
+  //   const imagestyle = {
+  //     width: "100vw",
+  //   };
+
+  return (
+    <div className={classes.image}>
+      <img src={logo} alt="home" />
+    </div>
+  );
 }
 
 export default Home;
