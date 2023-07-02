97458fdffcc89b7701ddb34a10aca45ad86e399f
joao <joaoflach@gmail.com>
2023-07-02 11:13:21 -0300

    showing full students information in a card

---
+++ src/components/students/FullStudentItem.js
@@ -0,0 +1,40 @@
+import Card from "../ui/Card";
+
+import classes from "./FullStudentItem.module.css";
+
+function FullStudentItem(props) {
+  return (
+    <Card>
+      <div className={classes.container}>
+        <div className={classes.grid}>
+          <div className={classes.image}>
+            <img src={props.student.foto} alt={props.student.nome} />
+          </div>
+          <div className={classes.content}>
+            <h2 align="left">
+              {props.student.nome + " " + props.student.sobrenome}
+            </h2>
+            <h3 align="left">E-mail: {props.student.email}</h3>
+            <h3 align="left">Telefone: {props.student.telefone}</h3>
+            <h3 align="left">Endereço: {props.student.endereço}</h3>
+            <h3 align="left">Data de Nascimento: {props.student.nascimento}</h3>
+          </div>
+        </div>
+        <div className={classes.gridbutton}>
+          <div className={classes.actions}>
+            <button className="btn" onClick={props.onClose}>
+              Editar
+            </button>
+          </div>
+          <div className={classes.actions}>
+            <button className="btn" onClick={props.onClose}>
+              Fechar
+            </button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default FullStudentItem;
--- src/components/students/StudentItem.js
+++ src/components/students/StudentItem.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 import Card from "../ui/Card";
+import FullStudentItem from "./FullStudentItem";
+import Backdrop from "../ui/Backdrop";
 
 import classes from "./StudentItem.module.css";
 
 function StudentItem(props) {
+  
+  const [showFullStudent, setShowFullStudent] = useState(false);
+  
+  function showStudentHandler() {
+    setShowFullStudent(true);
+  }
+
+  function closeShowStudentHandler() {
+    setShowFullStudent(false);
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -11,14 +26,22 @@ function StudentItem(props) {
             <img src={props.student.foto} alt={props.student.nome} />
           </div>
           <div className={classes.content}>
-            <h2 align="left">{props.student.nome + " " + props.student.sobrenome}</h2>
+            <h2 align="left">
+              {props.student.nome + " " + props.student.sobrenome}
+            </h2>
             <h3 align="left">{props.student.email}</h3>
           </div>
           <div className={classes.actions}>
-            <button>Ver</button>
+            <button className="btn" onClick={showStudentHandler}>
+              Ver
+            </button>
           </div>
         </div>
       </Card>
+      
+      {showFullStudent? <FullStudentItem student={props.student} onClose={closeShowStudentHandler}/> : null}
+      {showFullStudent? <Backdrop onClick={closeShowStudentHandler}/> : null}
+
     </li>
   );
 }
