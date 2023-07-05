651ab95ff72b5cf928a9689b46219ddd63b878cb
joao <joaoflach@gmail.com>
2023-07-05 09:06:47 -0300

    created custom hook for the authentication

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,58 +1,17 @@
-import React, { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { createContext } from "react";
 
-import { authenticate, signup } from "../api/api";
+import useAuth from "./hooks/useAuth";
 
 const Context = createContext();
 
 function AuthProvider(props) {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      setAuthenticated(true);
-    }
-
-    setLoading(false);
-  }, []);
-
-  async function signinHandler(authData) {
-    const data = await authenticate(authData);
-
-    if (data.message === "User authenticated successfully") {
-      localStorage.setItem("user", JSON.stringify(data.user));
-      localStorage.setItem("token", data.access_token);
-      setAuthenticated(true);
-      navigate("/home");
-    } else {
-      alert("Email ou senha incorretos.");
-    }
-  }
-
-  async function signupHandler(authData) {
-    const data = await signup(authData);
-
-    if (data.message === "User authenticated successfully") {
-      localStorage.setItem("user", JSON.stringify(data.user));
-      localStorage.setItem("token", data.access_token);
-      setAuthenticated(true);
-      alert("Cadastrado com sucesso. Por favor, faça o login.");
-      navigate("/signin");
-    } else {
-      alert("Informações incorretas. Erro: " + JSON.stringify(data));
-    }
-  }
-
-  function logoutHandler() {
-    setAuthenticated(false);
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    navigate("/signin");
-  }
+  const {
+    loading,
+    authenticated,
+    signinHandler,
+    signupHandler,
+    logoutHandler,
+  } = useAuth();
 
   return (
     <Context.Provider
---
+++ src/context/hooks/useAuth.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { authenticate, signup } from "../../api/api";
+
+function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setAuthenticated(true);
+    }
+
+    setLoading(false);
+  }, []);
+
+  async function signinHandler(authData) {
+    const data = await authenticate(authData);
+
+    if (data.message === "User authenticated successfully") {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", data.access_token);
+      setAuthenticated(true);
+      navigate("/home");
+    } else {
+      alert("Email ou senha incorretos.");
+    }
+  }
+
+  async function signupHandler(authData) {
+    const data = await signup(authData);
+
+    if (data.message === "User authenticated successfully") {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", data.access_token);
+      setAuthenticated(true);
+      alert("Cadastrado com sucesso. Por favor, faça o login.");
+      navigate("/signin");
+    } else {
+      alert("Informações incorretas. Erro: " + JSON.stringify(data));
+    }
+  }
+
+  function logoutHandler() {
+    setAuthenticated(false);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    navigate("/signin");
+  }
+
+  return {
+    loading,
+    authenticated,
+    signinHandler,
+    signupHandler,
+    logoutHandler,
+  };
+}
+
+export default useAuth;
